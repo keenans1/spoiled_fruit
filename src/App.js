2781ea3530c9fd5db913b2a7b2a9c4cf9ac705a7
Keenan Southall <keenan.southall@yahoo.com>
2023-02-03 15:07:31 -0700

    Set the movies state to sample movies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import './App.css';
+import AllMovies from './AllMovies'
+import movieData from './movieData'
 
-class App extends Component() {
+class App extends Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ movies: movieData })
   }
 
   render() {
+
+    return (
+      <main>
+        <AllMovies />
+      </main>
+    )
   }
 }
 
+
+
 export default App;
+
+// import the movies 
+// set the state
+// pass as prop to ALlMovies
