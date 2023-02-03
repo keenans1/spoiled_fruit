ee5e0ff89be6acfbec885b597df380033dcaf4b8
Angela Wirth <angiewirth92@gmail.com>
2023-02-03 17:53:05 -0500

    Create display functionality and styling

--- src/AllMovies.js
+++ src/AllMovies.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import './AllMovies.css'
 
 const AllMovies = (props) => {
-  const movies = props.movies.map(movie => movie.title)
+  const movies = props.movies.map(movie => {
+    return (
+        <h2 className="font">{movie.title}</h2>
+    )
+  })
   return (
-   <div>
-    <h2>{movies}</h2>
-   </div>
-  )
+        <div className='movies-container'>
+          {movies}
+        </div>
+      )
 
 }
 
 export default AllMovies
+
+
+// const Ideas = ({ideas}) => {
+
+//   const ideaCards = ideas.map(idea => {
+//     return (
+//       <Card
+//         title={idea.title}
+//         description={idea.description}
+//         id={idea.id}
+//         key={idea.id}
+//       />
+//     )
+//   })
+
+//   return (
+//     <div className='ideas-container'>
+//       {ideaCards}
+//     </div>
+//   )
+// }
