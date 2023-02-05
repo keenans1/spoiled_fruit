63f94ce69b1f782429d81b7b27a688e0b9b677e2
Angela Wirth <angiewirth92@gmail.com>
2023-02-04 19:47:39 -0500

    Add div for movie title on all movie display to align movies horizontally

--- src/Movie.js
+++ src/Movie.js
@@ -4,10 +4,12 @@ import './Movie.css'
 const Movie = (props) => {
     const display = props.movieClicked ? 'hidden' : 'movie-container'
     return (
-        <div className={display} onClick={() => props.onClick(props.movie.id)}>
-            <h2 className="font title">{props.movie.title}</h2>
-            <img src={props.movie.poster_path} alt='movie poster' />
+      <div className={display} onClick={() => props.onClick(props.movie.id)}>
+        <div className='title-container'>
+          <h2 className="font title">{props.movie.title}</h2>
         </div>
+          <img src={props.movie.poster_path} alt='movie poster' />
+      </div>
     )
 }
 
