2c6c2d64cddccc4422cd10942bd708a4f9c4a55b
Keenan Southall <keenan.southall@yahoo.com>
2023-02-07 18:48:06 -0700

    Refactor MovieInfo to class based component

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './MovieInfo.css'
-const MovieInfo = (props) => {
-  const {title, poster_path, runtime, overview, revenue, release_date, average_rating} = props.selectedMovie
-  return (
-    <div className='single-container'>
-      <h3 className='font movie-title'>{title}</h3>
-      <img className='font margin' src={poster_path} alt={title}/>
-      <h4 className='font'>Runtime: {runtime} minutes</h4>
-      <h4 className='font margin'>Rating: {average_rating}/10</h4>
-      <h4 className='font margin'>Overview: {overview}</h4>
-      <h4 className='font margin'>Release Date: {release_date}</h4>
-      <h4 className='font'>Revenue: ${revenue}</h4>
-      <button className='button' onClick={() => props.returnHome()} >Home</button>
-    </div>
-  )
+class MovieInfo extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+
+
+  //   const { title, poster_path, runtime, overview, revenue, release_date, average_rating } = props.selectedMovie
+  // return (
+  //   <div className='single-container'>
+  //     <h3 className='font movie-title'>{title}</h3>
+  //     <img className='font margin' src={poster_path} alt={title} />
+  //     <h4 className='font'>Runtime: {runtime} minutes</h4>
+  //     <h4 className='font margin'>Rating: {average_rating}/10</h4>
+  //     <h4 className='font margin'>Overview: {overview}</h4>
+  //     <h4 className='font margin'>Release Date: {release_date}</h4>
+  //     <h4 className='font'>Revenue: ${revenue}</h4>
+  //     <button className='button' onClick={() => props.returnHome()} >Home</button>
+  //   </div>
+  //)
 }
 
 
