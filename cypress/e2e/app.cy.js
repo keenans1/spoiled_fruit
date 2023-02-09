a5262820e0804f4bbdcf287cbdf0a5bd25f1f1de
Angela Wirth <angiewirth92@gmail.com>
2023-02-08 20:53:58 -0500

    Pass all tests

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -30,14 +30,14 @@ describe('template spec', () => {
       }
     ]
   }
+  cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", movies )
 })
 
   it('Should visit our home page and display Spoiled Fruit title', () => {
     cy.contains('Spoiled Fruit')
   })
 
-  it('Should show a list of movie when a user visits the home page', () => {
-    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", movies )
+  it('Should show a list of movies when a user visits the home page', () => {
     cy.get('a[href="/movies/436270"]').should('exist')
     cy.get('a[href="/movies/724495"]').should('exist')
     cy.get('a[href="/movies/1013860"]').should('exist')
@@ -45,6 +45,40 @@ describe('template spec', () => {
 
   it('Should take a user to a different url endpoint and be shown that movie\'s info', () => {
     cy.get('a[href="/movies/436270"]').click()
+    cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270", {
+      movie: 
+    {
+      id: 436270,
+      title: "Black Adam",
+      poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+      backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+      release_date: "2022-10-19",
+      overview: "Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.",
+      genres: 
+    [
+      "Action",
+      "Fantasy",
+      "Science Fiction"
+      ],
+      budget: 200000000,
+      revenue: 384571691,
+      runtime: 125,
+      tagline: "The world needed a hero. It got Black Adam.",
+      average_rating: 4
+      }
+    })
+    cy.contains('Black Adam')
+    cy.contains('Runtime: 125 minutes')
+    cy.url().should('include', '/movies/436270')
+  })
+
+  it('Should take a user back to the dashboard when the home button is clicked', () => {
+    cy.get('a[href="/movies/436270"]').click()
+    cy.get('button').click()
+    cy.get('a[href="/movies/436270"]').should('exist')
+    cy.get('a[href="/movies/724495"]').should('exist')
+    cy.get('a[href="/movies/1013860"]').should('exist')
+    cy.url().should('include', '/')
   })
 
 })
