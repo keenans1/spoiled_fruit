19befc7311c497ee1872efdf71401ac5b71dfa09
Keenan Southall <keenan.southall@yahoo.com>
2023-02-11 13:51:46 -0700

    Update search conditional render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css';
 import AllMovies from './AllMovies'
 import MovieInfo from './MovieInfo'
@@ -32,8 +33,8 @@ class App extends Component {
     this.setState({ movieTitle: event.target.value })
   }
 
-  handleClick = (event) => {
-    event.preventDefault()
+  handleClick = () => {
+    //event.preventDefault()
     if (this.state.movieTitle) {
       const lowerCaseSearch = this.state.movieTitle.toLowerCase()
       const filtered = this.state.movies
@@ -47,7 +48,11 @@ class App extends Component {
     } else {
       this.setState({ filteredMovies: [] })
     }
-    this.setState({movieClicked: false, selectedMovie: null})
+    this.setState({ movieClicked: false, selectedMovie: null })
+
+    // const noMovies = ReactDOM.createRoot(document.getElementById('oops'))
+
+    // (this.state.movieTitle && this.state.filteredMovies.length === 0 && noMovies.classList.remove('hidden'))
   }
 
   render() {
@@ -56,7 +61,7 @@ class App extends Component {
       <main className='App'>
         <header className='header'>
           <div className='button-container'>
-            <NavLink to='/'><button className='button' onClick={() => this.setState({filteredMovies: [], movieTitle: '', movieClicked: false, selectedMovie: null})}>Home</button></NavLink>
+            <NavLink to='/'><button className='button' onClick={() => this.setState({ filteredMovies: [], movieTitle: '', movieClicked: false, selectedMovie: null })}>Home</button></NavLink>
           </div>
           <h1 className='title'>Spoiled Fruit</h1>
           <form>
@@ -67,14 +72,39 @@ class App extends Component {
               value={this.state.movieTitle}
               onChange={(event) => this.handleChange(event)}
             />
-            <NavLink to='http://localhost:3000'><button onClick={(event) => this.handleClick(event)}>Search</button></NavLink>
+            <NavLink to='/'><button onClick={() => this.handleClick()}>Search</button></NavLink>
           </form>
         </header>
         {this.state.isLoading && !this.state.errorMessage ? <h2>Loading</h2> : null}
         {!this.state.isLoading && this.state.errorMessage ? <h2>{this.state.errorMessage}</h2> : null}
-        {this.state.filteredMovies.length > 0 ? <Route exact path='/' render={() => <AllMovies movies={this.state.filteredMovies} />} ></Route> :
-        <Route exact path='/' render={() => <AllMovies movies={this.state.movies} />} ></Route>
-         }
+        {/* {this.state.filteredMovies.length > 0 ? <Route exact path='/' render={() => <AllMovies movies={this.state.filteredMovies} />} ></Route> :
+          <Route exact path='/' render={() => <AllMovies movies={this.state.movies} />} ></Route>
+        }
+
+        {this.state.movieTitle && this.state.filteredMovies.length === 0 ? <h2>oopsy</h2> : null} */}
+
+        {this.state.filteredMovies.length > 0 && <Route exact path='/' render={() => <AllMovies movies={this.state.filteredMovies} />} ></Route>}
+
+        {!this.state.movieTitle && this.state.filteredMovies.length === 0 && <Route exact path='/' render={() => <AllMovies movies={this.state.movies} />} ></Route>}
+
+        {this.state.movieTitle && this.state.filteredMovies.length === 0 && <h2 className='noMoviesError'>oopsy</h2>}
+
+
+
+
+
+        {/* {
+        
+        if() {
+
+        } else if() {
+
+        } else {
+
+        }
+        
+        } */}
+
         <Route path='/movies/:id' render={({ match }) => {
           return (
             <div className='info-container'>
