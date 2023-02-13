837332c24c5d5eb5c94d88fd6bab89dc96a39138
Keenan Southall <keenan.southall@yahoo.com>
2023-02-13 10:14:50 -0700

    Implement PropTypes

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Movie.css'
 import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const Movie = (props) => {
   return (
@@ -16,3 +17,18 @@ const Movie = (props) => {
 }
 
 export default Movie
+
+// destructure props
+// delete movieData.js file
+// probably delete setUpTests.js file
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    backdrop_path: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    average_rating: PropTypes.number,
+    release_date: PropTypes.string
+  })
+}
