17c7bddc19ac0fb73ba18794780003f5a6e0cc5b
Keenan Southall <keenan.southall@yahoo.com>
2023-02-13 10:02:49 -0700

    Implement PropTypes

--- src/AllMovies.js
+++ src/AllMovies.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './AllMovies.css'
 import Movie from './Movie.js'
+import PropTypes from 'prop-types'
 
 const AllMovies = (props) => {
-  const sorted = props.movies.sort((a,b) => {
-    if(a.title < b.title) {
+  const sorted = props.movies.sort((a, b) => {
+    if (a.title < b.title) {
       return -1
     }
-    if(a.title > b.title) {
+    if (a.title > b.title) {
       return 1
     }
     return 0
@@ -29,3 +30,44 @@ const AllMovies = (props) => {
 }
 
 export default AllMovies
+
+AllMovies.propTypes = {
+
+
+  movies: PropTypes.arrayOf(
+
+    PropTypes.shape(
+
+      {
+        id: PropTypes.number.isRequired,
+        poster_path: PropTypes.string,
+        backdrop_path: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        average_rating: PropTypes.number,
+        release_date: PropTypes.string
+      }
+    )
+
+
+  )
+
+
+
+  // Pokemon.propTypes = {
+  //   pokemon: PropTypes.shape({
+  //     name: PropTypes.string,
+  //     id: PropTypes.number,
+  //     base_stamina: PropTypes.number,
+  //     base_defense: PropTypes.number
+  //   })
+  // }
+
+
+  // id: PropTypes.number.isRequired,
+  // poster_path: PropTypes.string,
+  // backdrop_path: PropTypes.string,
+  // title: PropTypes.string.isRequired,
+  // average_rating: PropTypes.number,
+  // release_date: PropTypes.string
+
+}
